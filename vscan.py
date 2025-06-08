93cdce46522e73ac6d42a3becfb815f7ab5e159f
Nick Kokenis <kokenisnick18@gmail.com>
2025-06-08 08:29:17 -0700

    0.2.1 - Just some basic formatting changes

--- vscan.py
+++ vscan.py
@@ -4,6 +4,12 @@ import socket
 import argparse
 from datetime import datetime
 
+# Function:
+# Parse Ports
+#
+# Description:
+# A helper function for input_handler() that parses the ports specified by
+# the user during input
 def parse_ports(port_input):
     ports = set()
     if '-' in port_input:
@@ -19,15 +25,17 @@ def parse_ports(port_input):
 # Scan Ports
 #
 # Description:
-# A basic port scanner to see which ports, from the ones specified by the user, are open and what services are
-# running on those open ports
-def scan_ports(host, ports, timeout):
+# A basic port scanner that returns all open ports for a given host
+def scan_ports(host, ports, timeout, retries, verbose):
     open_ports = []
     for port in ports:
         try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sckt:
-                sckt.settimeout(timeout)
-                result = sckt.connect_ex((host, port))
+            # Initialize a socket stream
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sc:
+                sc.settimeout(timeout)
+                # Connect to the remote host on the current port
+                result = sc.connect_ex((host, port))
+                # If we get a response, the port is open
                 if result == 0:
                     print(f"[+] Port {port} is open")
                     open_ports.append(port)
@@ -57,14 +65,17 @@ def input_handler():
 
     # Create a mutually exclusive argument group for specifying ports to be scanned. At least one of the arguments -p or -pA must be included
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-p", type=str, dest="ports", help="Scan a single port (ex. 22), list of ports (ex. 22,80,443), or range of ports(ex. 1-65535).")
-    group.add_argument("-pA", action="store_true", dest="scan_all_ports", help="Scan all ports and all protocols.")
+    # Custom argument name for -p is ports
+    group.add_argument("-p", dest="ports", help="Scan a single port (ex. 22), list of ports (ex. 22,80,443), or range of ports(ex. 1-65535)")
+    # Custom argument name for -pA is scan_all_ports
+    group.add_argument("-pA", action="store_true", dest="scan_all_ports", help="Scan all ports and all protocols")
 
     # Add optional arguments
-    parser.add_argument("-v", "--verbose", action="store_true", help="Tells the program to print more verbose output, similar to Nmap.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Tells the program to print more verbose output, similar to Nmap")
     parser.add_argument("--max-retries", type=int, default=1, help="Maximum number of retries to connect to any given host before giving up. Default = 1")
     parser.add_argument("--max-timeout", type=int, default=20, help="Maximum amount of time, in seconds, to try and scan any given host before giving up. Default = 20 seconds")
 
+    # Parse user input
     args = parser.parse_args()
 
     # Parse port input
@@ -77,15 +88,15 @@ def input_handler():
             print("[-] Invalid port format. Use port number (ex. 22), port list (ex. 22,80,443), or 1-1024")
             exit()
     
-    # Scan
+    # Scan ports
     try:
         scan_ports(args.host, ports, args.max_timeout)
     except KeyboardInterrupt:
         print(f"[!] Scan cancelled")
     except Exception as e:
-        print(f"[!] An error occured: {e}")
+        print(f"[!] Error: {e}")
     finally:
-        print("Scan completed.")
+        print("[~] Scan completed")
     return
 
 # Function:
